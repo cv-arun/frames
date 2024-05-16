571ebfc62147df7a50526295ca7d51345d6fdda7
Arun <cvaruncv1998@gmail.com>
2024-05-16 22:36:44 +0530

    samll fixes

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -27,7 +27,7 @@ function Login() {
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center bg-[#f2f2f2]  ">
 
-      <div className="h-[400px] w-[30%] bg-white text-center flex flex-col items-center gap-10 p-10 rounded shadow-md text-black ">
+      <div className="h-[400px] md:w-[30%] bg-white text-center flex flex-col items-center gap-10 p-10 rounded shadow-md text-black ">
         <h1 className="text-4xl mb-15">Login</h1>
         <input className="p-2 bg-white rounded-md border-b-2 outline-none" name="email" onChange={handleData} placeholder='Email' />
         <input className="p-2 bg-white rounded-md border-b-2 outline-none" name="password" onChange={handleData} placeholder="Password" />
